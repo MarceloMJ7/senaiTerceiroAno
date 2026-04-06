838e4e6669e6d5d6ff20720956fd2c7a2aa63c5d
Marcelo de Moura Faria Costa <marcelo_mj7@hotmail.com>
2026-04-06 20:21:17 -0300

    att

--- MySQL/03 - Manipulação de Dados - DLM/insercao_dados.sql
+++ MySQL/03 - Manipulação de Dados - DLM/insercao_dados.sql
@@ -5,7 +5,35 @@ email varchar(100) NOT NULL,
 cargo varchar(100) NOT NULL);
 
 INSERT INTO funcionarios(nome, email, cargo)
-VALUES ("Cristiano Ronaldo", "Cristiano_Ronaldo@hotmail.com", "Jogador");
+VALUES ("Cristiano Ronaldo", "Cristiano_Ronaldo@hotmail.com", "Jogador"),
+ ("Cristiano", "Cristiano_Ronaldo@hotmail.com", "player"),
+("Ronaldo", "CristianoRonaldo@hotmail.com", "Jogador3"),
+("Ronaldinho", "Cristiano_o@hotmail.com", "Jogador2"),
+ ("Messi", "Crino_Ronaldo@hotmail.com", "Jogador4"),
+("Neymar", "Criso_Ronaldo@hotmail.com", "Jogador1"),
+("Bale", "Cristio_Ronaldo@hotmail.com", "Jogador5");
+
+
+
+ALTER TABLE funcionarios ADD COLUMN cidade varchar(100) DEFAULT "não informado";
+INSERT INTO funcionarios (nome, email,cargo)
+VALUES ("Ronaldinho", "cristianinho@outlook.com", "tec");
+SELECT * FROM funcionarios WHERE cargo  = "tec";
+
+SELECT * FROM funcionarios ORDER BY nome ASC;
+SELECT * FROM funcionarios LIMIT 3;
+SELECT DISTINCT cidade FROM funcionarios;
+
+SELECT cargo, COUNT(*) AS total_funcionarios
+FROM funcionarios GROUP BY cargo;
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE funcionarios SET cargo = "Desempregado" WHERE nome = "Ronaldo";
+
+
+
+
+
 
 
 
